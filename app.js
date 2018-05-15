16ece2a1f3f51f4733f430c067cef3592bce2978
Travis <travisjw@gmail.com>
2018-05-15 14:11:09 -0700

    have a chart appearing

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var itemsBeingShown = [];
 var itemsToTestAgainst = [];
 var displayTable = document.getElementById('displaytable');
 var resultsList = document.getElementById('resultslist');
-var StopAfterXVotes = 25;
-var tableSection = document.getElementById('display-table-section');
+var StopAfterXVotes = 3;
 
 //   ====================      Sales Item Constructor Function       =======================
 function SalesItem(imgFilepath, itemDescription, itemName) {
@@ -98,7 +97,7 @@ function buildDisplayTable() {
 }
 
 //   ==========    Event Listener Second Try:    ===========
-tableSection.addEventListener('click', buttonWasClicked);
+displayTable.addEventListener('click', buttonWasClicked);
 
 function buttonWasClicked(event){
     //create test array to make sure we're seeing clicks on buttons
@@ -109,16 +108,17 @@ function buttonWasClicked(event){
 
     //Count Vote
     var indexidnum = event.target.id;
-
+    
     if(testArray.includes(1*indexidnum)) 
     {
-    
-        itemsBeingShown[indexidnum].totalVotesForItem++;
         votesMadeThusFar++;
+        itemsBeingShown[indexidnum].totalVotesForItem++;
+        
 
         if(votesMadeThusFar===StopAfterXVotes)
         {
             endpage();
+            
         }
         else{
             selectNewObjects();
@@ -130,41 +130,86 @@ function buttonWasClicked(event){
 
 //  =============     =============     =============     =============     =============     
 
-new SalesItem('images/bag.jpg', 'R2Dbag!', 'Starwars Travel Bag');
-new SalesItem('images/banana.jpg', 'Banana Rama Ding Dong', 'Banana Slicer');
-new SalesItem('images/bathroom.jpg', 'You can Twitter, while on the shitter.', 'Poopy Pad');
-new SalesItem('images/breakfast.jpg', 'All in 1, Breakfast Maker', 'Breakfast');
-new SalesItem('images/bubblegum.jpg', "It's less gross than you think!  (Hopefully)", 'Balls.  Meat Balls.');
-new SalesItem('images/chair.jpg', "It's probably not the most uncomfortable chair in the world", "The Most Uncomfortable Chair in the World");
-new SalesItem('images/cthulhu.jpg', "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtan", "R'lyeh wgah'nagl");
-new SalesItem('images/dog-duck.jpg', 'Given how successfull cat-dog was, can you really afford to pass on this', 'The worst sequel idea since Sharknado IV');
-new SalesItem("images/dragon.jpg", 'Tastes like Chicken', 'Canned chicken');
-new SalesItem('images/pen.jpg', 'For a REAL working lunch', 'Food poisoning' );
-new SalesItem('images/pet-sweep.jpg', "It's your fur I'm trying to clean up anyways, douchebag!", 'Dog Slippers');
-new SalesItem('images/shark.jpg', 'To stay deliciously warm...', 'Jaws');
-new SalesItem('images/sweep.png', "It's the only way he'll clean his college dorm", 'child abuse');
-new SalesItem('images/tauntaun.jpg', 'How warm is one of these things anyways?', 'Lukewarm');
-new SalesItem('images/unicorn.jpg',"100% genuine","Secretariat");
-new SalesItem('images/usb.gif', "Back that thing up", 'USB');
-new SalesItem('images/water-can.jpg','"Maybe I can beat the designer over the head with it?"', "watering can't")
-new SalesItem('images/wine-glass.jpg',"If you can't drink out of this, it's time to stop drinking",'Red red wine.')
+function initialize () {
+    new SalesItem('images/bag.jpg', 'R2Dbag!', 'Starwars Travel Bag');
+    new SalesItem('images/banana.jpg', 'Banana Rama Ding Dong', 'Banana Slicer');
+    new SalesItem('images/bathroom.jpg', 'You can Twitter, while on the shitter.', 'Poopy Pad');
+    new SalesItem('images/breakfast.jpg', 'All in 1, Breakfast Maker', 'Breakfast');
+    new SalesItem('images/bubblegum.jpg', "It's less gross than you think!  (Hopefully)", 'Balls.  Meat Balls.');
+    new SalesItem('images/chair.jpg', "It's probably not the most uncomfortable chair in the world", "The Most Uncomfortable Chair in the World");
+    new SalesItem('images/cthulhu.jpg', "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtan", "R'lyeh wgah'nagl");
+    new SalesItem('images/dog-duck.jpg', 'Given how successfull cat-dog was, can you really afford to pass on this', 'The worst sequel idea since Sharknado IV');
+    new SalesItem("images/dragon.jpg", 'Tastes like Chicken', 'Canned chicken');
+    new SalesItem('images/pen.jpg', 'For a REAL working lunch', 'Food poisoning' );
+    new SalesItem('images/pet-sweep.jpg', "It's your fur I'm trying to clean up anyways, douchebag!", 'Dog Slippers');
+    new SalesItem('images/shark.jpg', 'To stay deliciously warm...', 'Jaws');
+    new SalesItem('images/sweep.png', "It's the only way he'll clean his college dorm", 'child abuse');
+    new SalesItem('images/tauntaun.jpg', 'How warm is one of these things anyways?', 'Lukewarm');
+    new SalesItem('images/unicorn.jpg',"100% genuine","Secretariat");
+    new SalesItem('images/usb.gif', "Back that thing up", 'USB');
+    new SalesItem('images/water-can.jpg','"Maybe I can beat the designer over the head with it?"', "watering can't")
+    new SalesItem('images/wine-glass.jpg',"If you can't drink out of this, it's time to stop drinking",'Red red wine.')
 
-selectNewObjects();
-buildDisplayTable();
+    selectNewObjects();
+    buildDisplayTable();
+}
 
+initialize();
 
 //   ================    Endpage
 
 function endpage(){
     //Turn off the Event Listners
-    tableSection.removeEventListener('click', buttonWasClicked);
+    displayTable.removeEventListener('click', buttonWasClicked);
 
-    //Show the results
+    // =======  Show the results as a chart
+    
+    //build out arrays for chart data.
+    var xAxisLabels = [];
+    var yAxisValues = [];
+    var chartBGColor = ['red','orange','charcoal','rebeccapurple'];
+    var chartBorderColor = [];
     for (var i in arrayOfItems){
-        var newLI = document.createElement('li');
-        newLI.textContent = arrayOfItems[i].totalVotesForItem + ' votes for ' + arrayOfItems[i].itemName + '.  (displayed ' + arrayOfItems[i].timesShown +' times)';
-        resultslist.appendChild(newLI);
+        xAxisLabels[i] = arrayOfItems[i].itemName;
+        yAxisValues[i] = arrayOfItems[i].totalVotesForItem
+        chartBorderColor[i] = Math.floor(Math.random()*256);
     }
+
+
+
+
+    var ctx = document.getElementById("saleschart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: xAxisLabels,
+            datasets: [{
+                label: '# of Votes',
+                data: yAxisValues,
+                backgroundColor: chartBGColor,
+                borderColor: chartBorderColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
 }
 
 
+/* Your savvy marketing person has just informed you that just showing users the numbered data of the click percentages is not going to cut it. You need a visual representation to grab the user's attention.
+
+New Bus Mall Technical Goal
+Do today's work on a branch called chartjs.
+
+Using ChartJS, display your vote totals in a bar chart as instructed in the initial definition of the BusMall assignment. If you want to try some additional chart types, do so with some of your alternate data that you have calculated from your vote results. The size and look of your chart is up to you, just be sure to put it in the area beneath the three images. Hint: for this type of data, bar charts work well.
+
+Remember to submit this link on canvas so that your work can be graded! */
