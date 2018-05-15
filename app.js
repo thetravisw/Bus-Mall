aa8f9e42b4f6e59593116236cfa613f90eb9a5ea
Travis <travisjw@gmail.com>
2018-05-14 21:06:50 -0700

    Can see what was most recently clicked

--- app.js
+++ app.js
@@ -25,15 +25,14 @@ function selectNewObjects() {
         itemsToTestAgainst[i] = itemsBeingShown[i].itemName;
     } // creates an array to test against to ensure we don't duplicate items from last round.
 
-    var testName = itemsToTestAgainst[0];
-
     //For loop to create appropriate number of items
     //While to ensure item isn't in the array of prohibited items for this time around.
     for (var i = 0; i < numItemsToDisplay; i++) {
-        while (itemsToTestAgainst.includes(testName)) {
+        do {
             var index = Math.floor(Math.random() * arrayOfItems.length);
-            testName = arrayOfItems[index].itemName;
+            var testName = arrayOfItems[index].itemName;
         }
+        while (itemsToTestAgainst.includes(testName))
         itemsToTestAgainst.push(arrayOfItems[index].itemName);
         itemsBeingShown[i] = arrayOfItems[index];
     }
@@ -44,17 +43,18 @@ function selectNewObjects() {
 
 new SalesItem('images/bag.jpg', 'R2Dbag!', 'Starwars Travel Bag');
 new SalesItem('images/banana.jpg', 'Banana Rama Ding Dong', 'Banana Slicer');
-new SalesItem('images/bathroom.jpg', 'Bathroom Ipad Holder', 'poopy-pad');
+new SalesItem('images/bathroom.jpg', 'You can Twitter, while on the shitter.', 'poopy-pad');
 new SalesItem('images/breakfast.jpg', 'All in 1, Breakfast Maker', 'Breakfast');
 new SalesItem('images/bubblegum.jpg', 'All the flavor of an Italian Grandma', 'sounds disgusting');
-new SalesItem('images/chair.jpg', 'It\'s probably not the most uncomfortable chair in the world', 'Yeah it is.');
+new SalesItem('images/chair.jpg', "It's probably not the most uncomfortable chair in the world", "Yeah it is.");
 
 
-//  initialize by saying StarWars 
-for (var i = 0; i < numItemsToDisplay; i++) {
-    itemsBeingShown.push(arrayOfItems[0])
+//  initialize by saying StarWars.   Might wanna switch to empty SalesItem
+function initializePage() {
+    for (var i = 0; i < numItemsToDisplay; i++) {
+        itemsBeingShown.push(arrayOfItems[0]);
+    }
 }
-
 
 //  Function to build out the table
 function buildDisplayTable() {
@@ -64,31 +64,81 @@ function buildDisplayTable() {
 
     for (var i in itemsBeingShown) {
         //  Do the Image Row
-        var tdImages = document.createElement('td');
+        var tdImages = document.createElement('th');
+        // switched from td to th to make capturing clicks easier.
         var img = document.createElement("img");
         img.width = 200;
         img.src = itemsBeingShown[i].imgFilepath;
+
+        //assign the td an ID to be used in the event listener
+        tdImages.id = itemsBeingShown[i].itemName
+
         tdImages.appendChild(img);
         trImages.appendChild(tdImages);
 
+
         //Do the Description Row
-    var tdDescription = document.createElement('td');
-    tdDescription.textContent = itemsBeingShown[i].itemDescription;
-    tdDescription.width=200;
-    trDescription.appendChild(tdDescription);
+        var tdDescription = document.createElement('td');
+        tdDescription.textContent = itemsBeingShown[i].itemDescription;
+        tdDescription.width = 200;
+        trDescription.appendChild(tdDescription);
     }
 
     displayTable.appendChild(trImages);
     displayTable.appendChild(trDescription);
 }
 
+//  =======   Add Event Listener to Table:  ==========
+
+
+
+//Got some help from Stack Overflow on this function.
+//Specifically, learned what a node-list is, and how
+//to apply it to this particular problem.
+
+
+function listenAndLog() {
+    //  make a node-list of all the th
+    var tableImages = document.querySelectorAll('th')
+
+    // add an event listener to each node
+    tableImages.forEach( function (clickMe) {
+        clickMe.addEventListener('click', function (clicked) {
+
+            // get the ID of the TH that was clicked on.
+            var itemClicked = clicked.currentTarget;
+            var idItemClicked = itemClicked.id
+            console.log(idItemClicked);
+
+            // There's got to be a better way of doing this!
+
+            selectNewObjects();
+            buildDisplayTable();
+
+
+        });
+    });
+};
+
+initializePage();
+
+for (var j=1;j<26;j++){
 selectNewObjects();
 buildDisplayTable();
+listenAndLog();
+}
 
 
+// displayTable.addEventListener('click',function(){
+// console.log('clicked');
 
+// selectNewObjects();
+// })
+// imgEl2.addEventListener('click', function() {
+//     allPictures[picture2Index].clicked++;
+//     chooseNewPictures();
 
-//  build out the table
+//   });
 
 //  function to display results
 
@@ -106,8 +156,7 @@ buildDisplayTable();
 
 The thing you want to build today will select three random photos from the image directory and display them side-by-side-by-side in the browser window.
 In addition, you'll want to be able to receive clicks on those displayed images, and track those clicks for each image. You'll also want to track how many times each image is displayed, for statistical purposes.
-Upon receiving a click, three new non-duplicating random images need to be automatically displayed. In other words, the three images that are displayed should contain no duplicates, nor should they duplicate with any images that we displayed immediately before.
-To do this, you'll want a constructor function that creates an object associated with each image, and has (at a minimum) properties for the name of the image (to be used for display purposes), its filepath, the number of times it has been shown, and the number of times it has been clicked. You'll probably find it useful to create a property that contains a text string you can use as an ID in HTML.
+
 After 25 selections have been made, turn off the event listeners on the images (to prevent additional voting) and also display a list of the products with votes received with each list item looking like "3 votes for the Banana Slicer".
 Stretch Goals For This Lab
 Handle the display and voting for an arbitrary number of images
